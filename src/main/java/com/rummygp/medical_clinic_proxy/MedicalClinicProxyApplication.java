e2fa8b60ec27d05fc4e8355c5c4b14e07b2039e5
egorummy <adam.mokrzycki22@gmail.com>
2025-10-28 00:28:20 +0100

    Initial commit

---
+++ src/main/java/com/rummygp/medical_clinic_proxy/MedicalClinicProxyApplication.java
@@ -0,0 +1,13 @@
+package com.rummygp.medical_clinic_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalClinicProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalClinicProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rummygp/medical_clinic_proxy/MedicalClinicProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rummygp.medical_clinic_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
